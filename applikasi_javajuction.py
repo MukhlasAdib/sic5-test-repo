98106a1ffb2cec8b925ae7e4411d91dda8f239d7
AndriKusumaRamadhani <andribussi76@gmail.com>
2024-06-05 19:02:55 +0700

    applikasi tim javajunction

---
+++ applikasi_javajuction.py
@@ -0,0 +1,9 @@
+name_1=("Andri Kusuma Ramadhani")
+name_2=("Denis Hatta")
+name_3=("Ananta Ipsi Maulana")
+name_4=("Hikmah Kasyfi Saroir")
+
+print(name_1)
+print(name_2)
+print(name_3)
+print(name_4)
